2f39fb03b57632fdd4db4f9ed6bc25248c3ad502
boraates <boraatess@outlook.com>
2021-01-19 07:17:33 +0300

    weather app version 2.0

--- app/src/main/java/com/example/verilerialmakap/MainActivity.java
+++ app/src/main/java/com/example/verilerialmakap/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.verilerialmakap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +29,8 @@ public class MainActivity extends AppCompatActivity
     TextView description, temp, pressure,humidity, tempMin,tempMax,mainDescription,Feels_Like;
     EditText editText;
     String cityName;
+    Button search;
+    ImageView instantweathericon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -40,12 +46,24 @@ public class MainActivity extends AppCompatActivity
         tempMax = findViewById(R.id.tempmax);
         mainDescription = findViewById(R.id.main);
         Feels_Like = findViewById(R.id.tempmax2);
+        instantweathericon = findViewById(R.id.weathericon);
+        instantweathericon.setImageResource(R.drawable.cloudcomputing);
+
+        search = findViewById(R.id.searchbutton);
 
         editText = findViewById(R.id.editText);
         cityName = editText.getText().toString();
 
+        editText.setText("istanbul");
 
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VerierCed ced = new VerierCed();
+                ced.execute();
 
+            }
+        });
     }
 
     public void TakeData(View view)
@@ -55,22 +73,20 @@ public class MainActivity extends AppCompatActivity
 
         if (cityName.equals(""))
         {
-            Toast.makeText(MainActivity.this,"enter a city name",Toast.LENGTH_LONG);
-
-        }else {
-
+            Toast.makeText(MainActivity.this,"enter a city name",Toast.LENGTH_LONG).show();
+        }
+        else {
             cityName = editText.getText().toString();
         }
 
         VerierCed verierCed = new VerierCed();
         verierCed.execute();
 
-
     }
 
+    @SuppressLint("StaticFieldLeak")
     class VerierCed extends AsyncTask<Void, Void, String>
     {
-
         @Override
         protected String doInBackground(Void... voids)
         {
@@ -90,7 +106,6 @@ public class MainActivity extends AppCompatActivity
                 bufferedReader.close();
                 httpURLConnection.disconnect();
                 return result.toString();
-
             }
             catch (MalformedURLException e)
             {
@@ -98,7 +113,6 @@ public class MainActivity extends AppCompatActivity
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
             return null;
         }
 
@@ -106,8 +120,6 @@ public class MainActivity extends AppCompatActivity
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
-           //textView.setText(s);
-           // double kelvin = 273.15;
             try {
 
                 JSONObject jsonObject = new JSONObject(s);
@@ -135,39 +147,59 @@ public class MainActivity extends AppCompatActivity
                 float newTemp = kelvin - 273 ;
                 int newTemp2 = (int) newTemp;
                 String temperature2 = String.valueOf(newTemp2);
-                temp.setText(temperature2 + "˚C");
+                temp.setText("Now:"+" "+temperature2 + "˚C");
+
+                if (newTemp2<0){
+                    instantweathericon.setImageResource(R.drawable.igloo);
+                }
+                else if (newTemp2 > 0 && newTemp2 < 5) {
+                    instantweathericon.setImageResource(R.drawable.cold);
+                }
+                else if (mainDescription.getText().toString().equals("Weather:Clouds")) {
+                    instantweathericon.setImageResource(R.drawable.cloudyone);
+                }
+                else if (mainDescription.getText().toString().equals("Weather:Rain")) {
+                    instantweathericon.setImageResource(R.drawable.rainy);
+                }
+                else if (newTemp2 > 5 && newTemp2 < 10){
+                    instantweathericon.setImageResource(R.drawable.cloudy);
+                }
+                else if (newTemp2 > 10 && newTemp2 < 15){
+                    instantweathericon.setImageResource(R.drawable.rain);
+                }
+                else if (newTemp2 > 15 && newTemp2 < 20 ){
+                    instantweathericon.setImageResource(R.drawable.rainy);
+                }
+                else if (newTemp2 > 20) {
+                    instantweathericon.setImageResource(R.drawable.sun);
+                }
 
                 float kelvin1 = Float.parseFloat(temp_min1);
                 float newTemp_Min = kelvin1 - 273.15F;
                 int newTemp_Min2 = (int) newTemp_Min;
                 String temp_Min2 = String.valueOf(newTemp_Min2);
-                tempMin.setText("Minumum temperature :" + temp_Min2 + "˚C");
+                tempMin.setText("Minumum temperature:"+" "+ temp_Min2 + "˚C");
 
                 float kelvin2 = Float.parseFloat(temp_max1);
                 float newTemp_Max = kelvin2 - 273.15F;
                 int newTemp_Max2 = (int) newTemp_Max;
                 String temp_Max2 = String.valueOf(newTemp_Max2);
-                tempMax.setText("Maximum temperature :"+temp_Max2 + "˚C");
+                tempMax.setText("Maximum temperature:"+" "+temp_Max2 + "˚C");
 
                 float kelvin3 = Float.parseFloat(feels_like);
                 float newFellsLike = kelvin3 - 273.15F;
                 int newFellsLike2 = (int) newFellsLike;
                 String Feels_Like2 = String.valueOf(newFellsLike2);
-                Feels_Like.setText("Feels Like :"+Feels_Like2 + "˚C");
 
+                Feels_Like.setText("Feels Like:"+" "+Feels_Like2 + "˚C");
                 humidity.setText("%" + humidity1);
                 pressure.setText(pressure1 + "hPa");
-
-                description.setText("Description    :" + description1);
-                mainDescription.setText("Weather    :" + main);
-
-
+                description.setText("Description:" + description1);
+                mainDescription.setText("Weather:" + main);
 
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-
-
         }
     }
 
